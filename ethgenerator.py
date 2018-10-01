352bde2e616db299940053f820ac4096ce53b2e8
Germán Lugo <german.lugo@bitnibs.com>
2018-10-01 13:14:59 +0200

    Create QRgen.

--- ethgenerator.py
+++ ethgenerator.py
@@ -1,5 +1,6 @@
 import blocksmith
 import json
+import qrcode
 
 paper_wallet = {}
 
@@ -9,15 +10,13 @@ index = 0
 desired = 10
 while index < desired: 
     key = kg.generate_key()
-    #file.write(key + "\n")
     address = blocksmith.EthereumWallet.generate_address(key)
     print(address)
     checksum_address = blocksmith.EthereumWallet.checksum_address(address)
-    #file.write(checksum_address + "\n")
     paper_wallet[checksum_address] = "{}".format(key)
     index += 1
 
 json_data = json.dumps(paper_wallet)
-file = open('CTOK000001.txt', "w")
+file = open('CTOK000001.json', "w")
 file.write(json_data)
 file.close()
---
+++ qrgenerator.py
@@ -0,0 +1,24 @@
+import os
+import qrcode
+import qrcode.image.svg
+import json
+
+factory = qrcode.image.svg.SvgPathImage
+
+index = 1
+
+with open('CTOK000001.json', 'r') as data:
+    string_data = data.read()
+
+json_data = json.loads(string_data)
+for x in json_data:
+    print(x + ":", json_data[x])
+    pubkey = qrcode.make(x, image_factory = factory)
+    prvkey = qrcode.make(json_data[x], image_factory = factory)
+    if not os.path.exists("folder{}".format(index)):
+        os.makedirs("folder{}".format(index))
+        pubkey.save("pubkey{}.svg".format(index))
+        prvkey.save("prvkey{}.svg".format(index))
+        os.system("mv pubkey{}.svg folder{}".format(index, index))
+        os.system("mv prvkey{}.svg folder{}".format(index, index))        
+    index += 1
